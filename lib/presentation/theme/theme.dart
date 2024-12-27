09a5d7ae8d86601d2f911acd961d835a005d3b8f
Yevhenii Hliebov <yevheniy.hliebov@gmail.com>
2024-12-27 16:28:02 +0200

    feat: restructure app with new main screen and theme support

---
+++ lib/presentation/theme/theme.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:test_assignment_lanars/presentation/theme/color_schemes.dart';
+
+class AppTheme {
+  const AppTheme._();
+
+  static ThemeData get light {
+    return ThemeData(
+      colorScheme: lightColorScheme,
+    );
+  }
+  
+  static ThemeData get dark {
+    return ThemeData(
+      colorScheme: darkColorScheme,
+    );
+  }
+}
+

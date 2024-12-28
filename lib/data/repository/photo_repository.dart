a144f9c3b2d479ac0bc5eaaf486827ae11211efe
Yevhenii Hliebov <yevheniy.hliebov@gmail.com>
2024-12-28 19:53:06 +0200

    feat: implement PhotoRepository for fetching curated photos from Pexels API

---
+++ lib/data/repository/photo_repository.dart
@@ -0,0 +1,35 @@
+import 'package:dio/dio.dart';
+import 'package:retrofit/retrofit.dart';
+import 'package:test_assignment_lanars/data/models/photo.dart';
+
+part 'photo_repository.g.dart';
+
+@RestApi()
+abstract class PhotoRepository {
+  factory PhotoRepository(Dio dio, {String baseUrl = 'https://api.pexels.com/v1/'}) {
+    dio.options.headers = {
+      'Authorization': 'oAE0vUY1aWn1GwYyL1ex8nFWq74oBkh1MNPq35iRClsIbmc03co5gTdN',
+    };
+    return _PhotoRepository(dio, baseUrl: baseUrl);
+  }
+
+  @GET('/curated?per_page=50')
+  Future<PhotoResponse> getPhotos();
+}
+
+
+class PhotoResponse {
+  final List<Photo> photos;
+
+  PhotoResponse({
+    required this.photos,
+  });
+
+  factory PhotoResponse.fromJson(Map<String, dynamic> json) {
+    final photosJson = json['photos'] as List;
+    final photos = photosJson.map((json) {
+      return Photo.fromJson(json);
+    }).toList();
+    return PhotoResponse(photos: photos);
+  }
+}
---
+++ lib/data/repository/photo_repository.g.dart
@@ -0,0 +1,86 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'photo_repository.dart';
+
+// **************************************************************************
+// RetrofitGenerator
+// **************************************************************************
+
+// ignore_for_file: unnecessary_brace_in_string_interps,no_leading_underscores_for_local_identifiers,unused_element,unnecessary_string_interpolations
+
+class _PhotoRepository implements PhotoRepository {
+  _PhotoRepository(
+    this._dio, {
+    this.baseUrl,
+    this.errorLogger,
+  });
+
+  final Dio _dio;
+
+  String? baseUrl;
+
+  final ParseErrorLogger? errorLogger;
+
+  @override
+  Future<PhotoResponse> getPhotos() async {
+    final _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _headers = <String, dynamic>{};
+    const Map<String, dynamic>? _data = null;
+    final _options = _setStreamType<PhotoResponse>(Options(
+      method: 'GET',
+      headers: _headers,
+      extra: _extra,
+    )
+        .compose(
+          _dio.options,
+          '/curated?per_page=50',
+          queryParameters: queryParameters,
+          data: _data,
+        )
+        .copyWith(
+            baseUrl: _combineBaseUrls(
+          _dio.options.baseUrl,
+          baseUrl,
+        )));
+    final _result = await _dio.fetch<Map<String, dynamic>>(_options);
+    late PhotoResponse _value;
+    try {
+      _value = PhotoResponse.fromJson(_result.data!);
+    } on Object catch (e, s) {
+      errorLogger?.logError(e, s, _options);
+      rethrow;
+    }
+    return _value;
+  }
+
+  RequestOptions _setStreamType<T>(RequestOptions requestOptions) {
+    if (T != dynamic &&
+        !(requestOptions.responseType == ResponseType.bytes ||
+            requestOptions.responseType == ResponseType.stream)) {
+      if (T == String) {
+        requestOptions.responseType = ResponseType.plain;
+      } else {
+        requestOptions.responseType = ResponseType.json;
+      }
+    }
+    return requestOptions;
+  }
+
+  String _combineBaseUrls(
+    String dioBaseUrl,
+    String? baseUrl,
+  ) {
+    if (baseUrl == null || baseUrl.trim().isEmpty) {
+      return dioBaseUrl;
+    }
+
+    final url = Uri.parse(baseUrl);
+
+    if (url.isAbsolute) {
+      return url.toString();
+    }
+
+    return Uri.parse(dioBaseUrl).resolveUri(url).toString();
+  }
+}

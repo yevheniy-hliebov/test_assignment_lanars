b9618c5c0629241807590a4a23da9e85902309d6
Yevhenii Hliebov <yevheniy.hliebov@gmail.com>
2024-12-28 13:25:45 +0200

    feat: refactor login form and add validation logic

--- lib/presentation/widgets/login_form.dart
+++ lib/presentation/widgets/login_form.dart
@@ -1,15 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:test_assignment_lanars/utils/validation/validations/email_validation.dart';
+import 'package:test_assignment_lanars/utils/validation/validations/password_validation.dart';
+import 'package:test_assignment_lanars/utils/validation/validator.dart';
 
-class LoginForm extends StatelessWidget {
+class LoginForm extends StatefulWidget {
+  const LoginForm({super.key});
+
+  @override
+  State<LoginForm> createState() => _LoginFormState();
+}
+
+class _LoginFormState extends State<LoginForm> {
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
 
-  LoginForm({super.key});
+  String? _emailErrorText;
+  String? _passwordErrorText;
 
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
       child: Form(
+        key: formKey,
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: [
@@ -18,44 +31,85 @@ class LoginForm extends StatelessWidget {
               style: Theme.of(context).textTheme.headlineLarge,
             ),
             const SizedBox(height: 72),
-            TextFormField(
-              controller: emailController,
-              decoration: InputDecoration(
-                labelText: 'Email',
-                hintText: 'Enter your email',
-                helperText: ' ',
-                floatingLabelBehavior: FloatingLabelBehavior.always,
-              ),
-            ),
+            _buildEmailField(),
             const SizedBox(height: 16),
-            TextFormField(
-              controller: passwordController,
-              decoration: InputDecoration(
-                labelText: 'Password',
-                hintText: 'Enter your password',
-                helperText: ' ',
-                floatingLabelBehavior: FloatingLabelBehavior.always,
-              ),
-              obscureText: true,
-            ),
+            _buildPasswordField(),
             const SizedBox(height: 24),
-            SizedBox(
-              width: double.maxFinite,
-              child: FilledButton(
-                style: FilledButton.styleFrom(
-                    textStyle: TextStyle(
-                  fontSize: 14,
-                )),
-                onPressed: () {
-                  print('email ${emailController.text}');
-                  print('password ${passwordController.text}');
-                },
-                child: Text('Log in'),
-              ),
-            ),
+            _buildSubmitButton(),
           ],
         ),
       ),
     );
+  }
+
+  TextFormField _buildEmailField() {
+    return TextFormField(
+      controller: emailController,
+      decoration: InputDecoration(
+        labelText: 'Email',
+        hintText: 'Enter your email',
+        helperText: ' ',
+        errorText: _emailErrorText,
+        floatingLabelBehavior: FloatingLabelBehavior.always,
+      ),
+      onChanged: (_) => _clearEmailError(),
+    );
+  }
+
+  TextFormField _buildPasswordField() {
+    return TextFormField(
+      controller: passwordController,
+      decoration: InputDecoration(
+        labelText: 'Password',
+        hintText: 'Enter your password',
+        helperText: ' ',
+        errorText: _passwordErrorText,
+        floatingLabelBehavior: FloatingLabelBehavior.always,
+      ),
+      obscureText: true,
+      onChanged: (_) => _clearPasswordError(),
+    );
+  }
+
+  SizedBox _buildSubmitButton() {
+    return SizedBox(
+      width: double.maxFinite,
+      child: FilledButton(
+        style: FilledButton.styleFrom(textStyle: TextStyle(fontSize: 14)),
+        onPressed: onSubmit,
+        child: Text('Log in'),
+      ),
+    );
+  }
+
+  void _clearEmailError() {
+    setState(() {
+      _emailErrorText = null;
+    });
+  }
+
+  void _clearPasswordError() {
+    setState(() {
+      _passwordErrorText = null;
+    });
+  }
+
+  void onSubmit() {
+    String? emailErrorText =
+        Validator.applyOne(context, EmailValidation())(emailController.text);
+    String? passwordErrorText = Validator.applyOne(
+        context, PasswordValidation())(passwordController.text);
+
+    setState(() {
+      _emailErrorText = emailErrorText;
+      _passwordErrorText = passwordErrorText;
+    });
+
+    if (_emailErrorText == null && _passwordErrorText == null) {
+      print('email ${emailController.text}');
+      print('password ${passwordController.text}');
+    } else {
+      print('Form is not valid');
+    }
   }
 }
